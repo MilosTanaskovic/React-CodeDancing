7ed6bf25f4607e1e32e4628092630eb51be2d8c3
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-07-22 18:08:55 +0000

    Navbar component...

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,10 +1,15 @@
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <button className="btn">button</button>
-    </h1>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
