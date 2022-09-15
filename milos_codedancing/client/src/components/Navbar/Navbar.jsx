d86eac22a50af48eadeaab382da2ab64a8432a8a
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-09-15 19:20:59 +0200

    add assets...

--- milos_codedancing/client/src/components/Navbar/Navbar.jsx
+++ milos_codedancing/client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import {images} from '../../constants'
 
 import './Navbar.scss';
 
 const Navbar = () => {
+    const [nav, setNav] = useState([
+        'home',
+        'about',
+        'work',
+        'skills',
+        'contact',
+    ])
+    console.log(nav)
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='app__navbar'>
+         <div className='app__navbar-logo'>
+            <img src={images.logo} alt="logo" />
+        </div>
+        <ul className='app__navbar-links'>
+            {nav.map((item) => {
+                return (
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div/>
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                )
+            })}
+        </ul>
+    </nav>
   )
 }
 
