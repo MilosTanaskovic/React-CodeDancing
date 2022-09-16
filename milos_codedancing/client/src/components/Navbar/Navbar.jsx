801c298c715333fc112b74f5c4088b139a60cd0a
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-09-16 23:50:08 +0200

    Navbar Components [desctop, mobile] | scss

--- milos_codedancing/client/src/components/Navbar/Navbar.jsx
+++ milos_codedancing/client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion';
 
 import {images} from '../../constants'
 
@@ -11,13 +13,15 @@ const Navbar = () => {
         'work',
         'skills',
         'contact',
-    ])
+    ]);
+    const [toggle, setToggle] = useState(false);
     console.log(nav)
   return (
     <nav className='app__navbar'>
          <div className='app__navbar-logo'>
             <img src={images.logo} alt="logo" />
         </div>
+        {/* desk nav bar */}
         <ul className='app__navbar-links'>
             {nav.map((item) => {
                 return (
@@ -28,6 +32,26 @@ const Navbar = () => {
                 )
             })}
         </ul>
+        {/* mobile navigation bar */}
+        <div className="app__navbar-menu">
+            <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+            {toggle && (
+                <motion.div
+                    whileInView={{ x: [300, 0]}}
+                    transition={{duration: 0.85, ease: 'easeOut'}}
+                >
+                    <HiX onClick={() => setToggle(false)} />
+                    <ul>
+                    {nav.map((item) => (
+                        <li  key={item}>
+                            <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
+                        </li>
+                    ))}
+                    </ul>
+                </motion.div>
+            )}
+        </div>
     </nav>
   )
 }
