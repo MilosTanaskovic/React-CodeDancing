ebcf0134c51ad856a85e002752c223cd3afce48c
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-09-20 21:56:19 +0200

    Sanity Client and React Proj

--- milos_codedancing/client/src/container/About/About.jsx
+++ milos_codedancing/client/src/container/About/About.jsx
@@ -2,29 +2,37 @@ import React, {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 
 import {images} from '../../constants';
+import { client, urlFor } from '../../Client'
 
 import './About.scss';
 
-const aboutsData = [
-  {
-    'title' : 'web dev',
-    'description': 'good',
-    'imgUrl': images.about01,
-  },
-  {
-    'title' : 'web design',
-    'description': 'good',
-    'imgUrl': images.about02,
-  },
-  {
-    'title' : 'ux/ui',
-    'description': 'good',
-    'imgUrl': images.about03,
-  }
-]
+// const aboutsData = [
+//   {
+//     'title' : 'web dev',
+//     'description': 'good',
+//     'imgUrl': images.about01,
+//   },
+//   {
+//     'title' : 'web design',
+//     'description': 'good',
+//     'imgUrl': images.about02,
+//   },
+//   {
+//     'title' : 'ux/ui',
+//     'description': 'good',
+//     'imgUrl': images.about03,
+//   }
+// ]
 
 const About = () => {
-  const [abouts, setAbouts] = useState(aboutsData);
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, [])
   return (
     <>
       <h2 className="head-text">I Know That <span>Good Apps </span><br />means <span>Good Business</span></h2>
@@ -38,7 +46,7 @@ const About = () => {
             className='app__profiles-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: '20px'}}>{about.title}</h2>
             <p className="t-text" style={{marginTop: '10px'}}>{about.description}</p>
           </motion.div>
