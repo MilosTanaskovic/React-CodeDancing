e5bc6d8d94d5aed26c9c1b4e641d0ead1c8eb8d9
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-09-26 11:51:46 +0200

    HOC for motion div wrapper

--- milos_codedancing/client/src/container/Work/Work.jsx
+++ milos_codedancing/client/src/container/Work/Work.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import {AiFillEye, AiFillGithub} from 'react-icons/ai';
 import {motion} from 'framer-motion';
 
-import {AppWrap} from '../../wrapper';
+import {AppWrap, MotionWrap} from '../../wrapper';
 import {urlFor, client} from '../../Client';
 
 import './Work.scss';
@@ -114,4 +114,8 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work')
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  "app__primarybg"
+)
