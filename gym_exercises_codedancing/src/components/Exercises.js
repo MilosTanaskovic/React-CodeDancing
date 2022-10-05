498b0f1fbafc0e70f095bf28fb944c34e89d5dd2
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-10-05 17:02:06 +0200

    Pagination UI and Logic!

--- gym_exercises_codedancing/src/components/Exercises.js
+++ gym_exercises_codedancing/src/components/Exercises.js
@@ -6,7 +6,19 @@ import { fetchData, exercisesOptions } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({exercises, setExercises, bodyPart}) => {
-    console.log(exercises)
+    const [currentPage, setCurrentPage] = useState(1);
+    const exercisesPerPage = 9;
+
+    const indexOfLastExercise = currentPage * exercisesPerPage;
+    const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+    const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+    const handlePaginate = (e, value) => {
+        setCurrentPage(value);
+
+        window.scrollTo({top: 1800, behavior: 'smooth'});
+    }
+    
     return (
         <Box
             id="exercises"
@@ -26,12 +38,28 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
                 flexWrap="wrap"
                 justifyContent="center"
             >
-                {exercises.map((exercise, index) => (
+                {currentExercises.map((exercise, index) => (
                     <ExerciseCard 
                         key={index}
                         exercise={exercise}
                     />
                 ))}
+            </Stack>
+            <Stack
+                mt="100px"
+                alignItems="center"
+            >
+                {exercises.length > 9 && (
+                    <Pagination 
+                        color="standard"
+                        shape="rounded"
+                        defaultPage={1}
+                        count={Math.ceil(exercises.length / exercisesPerPage)}
+                        page={currentPage}
+                        onChange={handlePaginate}
+                        size="large"
+                    />
+                )}
             </Stack>
         </Box>
     )
