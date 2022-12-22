8afbbd1dd2b9e15bc9b4ec86255f31f9fafba762
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-12-22 09:47:18 +0100

    add gitignore

--- Working-With-Forms-CD/01-Form-Validation/src/components/SimpleInput.js
+++ Working-With-Forms-CD/01-Form-Validation/src/components/SimpleInput.js
@@ -1,9 +1,16 @@
+import React, {useState} from 'react';
+
 const SimpleInput = (props) => {
+  const [enterName, setEnterName] = useState('');
+
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input 
+          type='text' 
+          id='name' 
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
